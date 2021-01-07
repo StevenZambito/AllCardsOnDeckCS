5a7d02b04385793e48d3c266fc4e93747260ee99
StevenZambito <zambito.steven@icloud.com>
2021-01-06 20:19:49 -0500

    Generated card deck and displayed to user

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to 'All Cards On Deck'");
+
+            var deckOfCards = new List<string>();
+
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+            for (var i = 0; i < ranks.Count; i++)
+            {
+                for (var j = 0; j < suits.Count; j++)
+                {
+                    deckOfCards.Add($"{ranks[i]} of {suits[j]}");
+                }
+            }
+            Console.WriteLine("This is the deck of cards");
+
+            foreach (var card in deckOfCards)
+            {
+                Console.WriteLine(card);
+            }
+        }
     }
-  }
 }
+
